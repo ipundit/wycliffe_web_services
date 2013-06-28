f86e8d620ca3c75fb9aca91d6e48f0e35c68d750
Brian Morrow <brianjohnmorrow@hotmail.com>
2013-06-28 21:42:49 +0100

    Signed-off-by: Brian Morrow <brianjohnmorrow@hotmail.com>

--- payments/classes/JapanCreditBureau.php
+++ payments/classes/JapanCreditBureau.php
@@ -3,84 +3,111 @@ require_once 'classes/User.php';
 
 class JapanCreditBureau
 {
-	public function makePurchase($user, $purchase, $msg) {
-		$msg = "Stubbed out JapanCreditBureau class.";
-		return true;
-		
-		// fixme2: Implement this according to JCB interface
-		
-		require_once 'classes/Organization.php';
-		
-		$po = new Organization($purchase->po());
-		
+        public function makePurchase($user, $purchase, $msg) {
+
+                require_once 'classes/Organization.php';
+
+                $po = new Organization($purchase->po());
+
 $isTesting = 1;
-		if (isTesting) {
-			$url = "https://dev.psigate.com:7989/Messenger/XMLMessenger";
-		} else {
-			$url = "https://secure.psigate.com:7934/Messenger/XMLMessenger";
-		}
 
-		$XPost = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>
-			<Order><StoreID>$po->store_id($isTesting)</StoreID><Passphrase>$po->pass_phrase($isTesting)</Passphrase>
-			<Subtotal>".$purchase->amount()."</Subtotal><PaymentType>CC</PaymentType>
-			<CardAction>0</CardAction>
-			<CardNumber>".$purchase->creditCard()."</CardNumber>
-			<CardExpMonth>".$purchase->month()."</CardExpMonth>
-			<CardExpYear>".$purchase->year()."</CardExpYear>
-			<Bname>".$user->name()."</Bname>
-			<Baddress1>".$user->address()."</Baddress1>
-			<Baddress2>".$user->address2()."</Baddress2>
-			<Bcity>".$user->city()."</Bcity>
-			<Bprovince>".$user->state()."</Bprovince>
-			<Bpostalcode>".$user->postalCode()."</Bpostalcode>
-			<Bcountry>".$user->country()."</Bcountry>
-			<CustomerIP>".$_SERVER['REMOTE_ADDR']."</CustomerIP>
-			<Phone>".$user->phoneNumber()."</Phone>
-			<Email>".$user->emailAddress()."</Email>
-			<Item>
-				<ItemID>Donation</ItemID>
-				<ItemDescription>Donation through website</ItemDescription>
-				<ItemQty>1</ItemQty>
-				<ItemPrice>".$purchase->amount()."</ItemPrice>
-				<Option>
-					<Project>".$purchase->project()."</Project>
-				</Option>
-			</Item>";
-		$XPost = $XPost . "</Order>";
+                if (isTesting) {
+                        $url = "https://beta-jcbacqapi.pvbcard.com/WSJCBAcqAPI.asmx";
+                } else {
+                        $url = "https://jcbacqapi.pvbcard.com/WSJCBAcqAPI.asmx";
+                }
 
-		$ch = curl_init();
-		curl_setopt($ch, CURLOPT_HEADER, 0); // Don’t return the header, just the html
-		curl_setopt($ch, CURLOPT_URL, $url); // set url to post to
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER,1); // return into a variable
-		curl_setopt($ch, CURLOPT_TIMEOUT, 40); // times out after 40s
-		curl_setopt($ch, CURLOPT_POSTFIELDS, $XPost); // add POST fields
-		curl_setopt($ch, CURLOPT_CAINFO, dirname(__FILE__) . "/cacert.crt");
+                $XPost = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>
+                <soap:Envelope xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" xmlns:soap=\"http://schemas.xmlsoap.org/soap/envelope/\">
+                  <soap:Body>
+                    <SaleTransaction xmlns=\"http://WSJCBAcq.pvbcard.com/\">
+                      <pCommandString>" . this->makeCommandString($user, $purchase, $po, $isTesting) . "</pCommandString>
+                    </SaleTransaction>
+                  </soap:Body>
+                </soap:Envelope>";
 
-		$result = curl_exec($ch); // run the whole process
-		if (curl_errno($ch)) {
-			$msg = curl_error($ch);
-			return false;
-		} else {
-			curl_close($ch);
-		}
+                $ch = curl_init();
+                curl_setopt($ch, CURLOPT_HEADER, 0); // Don<92>t return the header, just the html
+                curl_setopt($ch, CURLOPT_URL, $url); // set url to post to
+                curl_setopt($ch, CURLOPT_RETURNTRANSFER,1); // return into a variable
+                curl_setopt($ch, CURLOPT_TIMEOUT, 40); // times out after 40s
+                curl_setopt($ch, CURLOPT_POSTFIELDS, $XPost); // add POST fields
+                curl_setopt($ch, CURLOPT_CAINFO, dirname(__FILE__) . "/cacert.crt");
+                curl_setopt($ch, CURLOPT_HTTPHEADER, array('SOAPAction: http://WSJCBAcq.pvbcard.com/SaleTransaction'));
 
-		$xml_parser = xml_parser_create();
-		xml_parser_set_option($xml_parser,XML_OPTION_CASE_FOLDING,0);
-		xml_parser_set_option($xml_parser,XML_OPTION_SKIP_WHITE,1);
-		xml_parse_into_struct($xml_parser, $result, $vals, $index);
-		xml_parser_free($xml_parser);
+                $result = curl_exec($ch); // run the whole process
+                if (curl_errno($ch)) {
+                        $msg = curl_error($ch);
+                        return false;
+                } else {
+                        curl_close($ch);
+                }
 
-		switch ($vals[$index['Approved'][0]]['value']) {
-		case "APPROVED":
-			$msg = $vals[$index['OrderID'][0]]['value'];
-			return true;
-		case "DECLINED":
-			$msg = "Your credit card was declined";
-			return false;
-		default:
-			$msg = $vals[$index['ErrMsg'][0]]['value'];
-			return false;
-		}
-	}
+                $xml_parser = xml_parser_create();
+                xml_parser_set_option($xml_parser,XML_OPTION_CASE_FOLDING,0);
+                xml_parser_set_option($xml_parser,XML_OPTION_SKIP_WHITE,1);
+                xml_parse_into_struct($xml_parser, $result, $vals, $index);
+                xml_parser_free($xml_parser);
+                
+				$responseString = $vals[$index['SaleTransactionResult'][0]]['value']
+				
+				$fields = this->shred($responseString);
+				
+				if ($fields["FIELD=39"] == "00") {
+						$msg = "All well!";
+                        return true;					
+				} else {
+						$msg = "Error : " . $fields["FIELD=39"];
+						return false;
+				}
+        }
+
+        private function makeCommandString($user, $purchase, $po, $isTest) {
+
+            return "FIELD=2&VALUE="  . $purchase->creditCard() . ";" .
+            	   "FIELD=4&VALUE="  . this->formatAmount($purchase->amount()) . ";" .
+            	   "FIELD=14&VALUE=" . this->formatExpiry($purchase->month(), $purchase->year()) . ";" .
+            	   "FIELD=41&VALUE=" . $po->terminal_id($isTest) . ";" .     
+            	   "FIELD=42&VALUE=" . $po->merchant_id($isTest);          	              	   
+        }
+        
+        private function formatAmount($amount) {
+        	
+        	$pos = strpos($amount, ".");
+        	
+        	if ($pos === FALSE) {
+        		return $amount . "00";
+        	} else {
+        		return str_replace(".", "", $amount);
+        	}
+        	
+        	return $amount;
+        }
+        
+        private function formatExpiry($month, $year) {
+        	
+        	if (strlen($month) === 1) {
+        		
+        		return "0" . $month . $year;         		
+        	} else {
+        	
+        		return $month . $year;        		
+        	}
+        }
+        
+        private function shred($responseString) {
+        	
+        	$shreddings = array();
+        	
+        	$fieldValuePairs = explode($responseString, ";");
+        	
+        	foreach ($fieldValuePairs as &$fieldValuePair) {
+        		$splitFieldAndValue = explode($fieldValuePair, "&");
+        		
+        		$shreddings[$splitFieldAndValue[0]] = $splitFieldAndValue[1];
+        	}
+        	
+        	return $shreddings;
+        }          
 }
 ?>
